0019e8ff9f14d76f00088d5552ecbcd70dbfc4fb
minzzjo <minxxjozz@gmail.com>
2022-11-16 13:15:47 +0900

    merge w/ main first

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import {
-  __userSignUp,
-  __userCheck,
-  __NickCheck,
-} from "../../src/redux/modules/userSlice";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import { __userSignUp,__userCheck,__NickCheck } from '../../src/redux/modules/userSlice'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
 const SignUp = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const account = useSelector((state) => state.account);
   const initialState = {
-    email: "",
+    userId: "",
     nickname: "",
     password: "",
-    passwordCheck: "",
+    passwordCheck:"",    
   };
+
+
   const [join, setJoin] = useState(initialState);
   const [IdValid, setIdValid] = useState(false);
   const [nickValid, setNickValid] = useState(false);
   const [PwValid, setPwValid] = useState(false);
   const [PwCValid, setPwCValid] = useState(false);
+
+
+
+
   const onChangeHandler = (event) => {
     const { name, value } = event.target;
     setJoin({ ...join, [name]: value });
@@ -31,61 +34,69 @@ const SignUp = () => {
     const regexId =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
     //이메일 체크
-    //https://velog.io/@gym/React-721
-    if (regexId.test(join.email)) {
+  //https://velog.io/@gym/React-721
+    if(regexId.test(join.userId)){
       setIdValid(true);
     } else {
       setIdValid(false);
     }
+
     const regexNick = /^[가-힣ㄱ-ㅎa-zA-Z0-9._-]{1,19}$/;
     if (regexNick.test(join.nickname)) {
       setNickValid(true);
     } else {
       setNickValid(false);
     }
+
     const regexPw = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,19}$/;
     if (regexPw.test(join.password)) {
       setPwValid(true);
     } else {
       setPwValid(false);
     }
+
     const regexPwC = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,19}$/;
     if (regexPwC.test(join.passwordCheck)) {
       setPwCValid(true);
     } else {
       setPwCValid(false);
     }
-  };
+  }
+
+
   const obj = {
-    email: join.email,
+    userId: join.userId,
     nickname: join.nickname,
     password: join.password,
     passwordCheck: join.passwordCheck,
-  };
-  const userIdCheck =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+  }
+
+  const userIdCheck =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
   //이메일 체크
   //https://velog.io/@gym/React-721
   const usernicknameCheck = /^[가-힣ㄱ-ㅎa-zA-Z0-9._]{1,19}$/;
   //글자수만제한 2~20
   const passwordCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,19}$/;
   //if (!regPass.test(password)) alert("영문, 숫자, 특수기호 조합으로 8-20자리 이상 입력해주세요.")
+
+
+
   const onSubmitHandler = (event) => {
-    event.preventDefault();
-    if (!userIdCheck.test(obj.email)) {
-      return alert("아이디 양식에 맞춰주세요");
+    event.preventDefault()
+    if(account.idCheck.status === 200 && account.nickCheck.status === 200){
+    if(!userIdCheck.test(obj.userId)){
+      return alert("아이디 양식에 맞춰주세요")
     }
-    if (!usernicknameCheck.test(obj.nickname)) {
-      return alert("닉네임 양식에 맞춰주세요");
-    }
-    if (!passwordCheck.test(obj.password)) {
-      return alert("비밀번호 양식에 맞춰주세요");
+
+    if(!usernicknameCheck.test(obj.nickname)){
+      return alert("닉네임 양식에 맞춰주세요")
     }
     if (!passwordCheck.test(obj.passwordCheck)) {
       return alert("비밀번호 양식에 맞춰주세요");
     }
-    if (obj.email === "" || obj.email === undefined) {
-      return alert("빈칸을 입력해주세요.");
+    
+    if(obj.userId === "" || obj.userId === undefined) {
+      return alert("빈칸을 입력해주세요.")
     }
     if (obj.nickname === "" || obj.nickname === undefined) {
       return alert("빈칸을 입력해주세요.");
@@ -96,137 +107,139 @@ const SignUp = () => {
     if (obj.passwordCheck === "" || obj.passwordCheck === undefined) {
       return alert("빈칸을 입력해주세요.");
     }
-    if (account.idCheck.status !== 200 || account.nickCheck.status !== 200) {
-      alert("중복확인을 해주세요");
-    }
+    
+  }
     // 중복확인이 true이고 true일때 그리고 dispatch를 보내서
-    // account statusCode 회원가입이 response로 왔을때 가입됨.
-    dispatch(__userSignUp(obj));
-    if (
-      account.idCheck.status === 200 &&
-      account.nickCheck.status === 200 &&
-      obj.password === obj.passwordConfirm
-    ) {
-      alert("회원가입이 완료되었습니다.");
-      window.location.replace("/SignIn");
+    // account statusCode 회원가입이 response로 왔을때 가입됨. 
+    
+    dispatch(__userSignUp(obj))
+    if(account.idCheck.status ===200 && account.nickCheck.status === 200 &&obj.password == obj.passwordConfirm){
+      alert("회원가입이 완료되었습니다.")  
+    window.location.replace("/SignIn")
     }
-  };
-  // useEffect(() => {
-  //   if(account.statusCode === 200){
-  //     alert("회원가입이 완료되었습니다.")
-  //     setJoin({
-  //       email : "",
-  //       nickname: "",
-  //       password: "",
-  //       passwordCheck: "",
-  //     })
-  //       window.location.replace("/SignIn")
-  //   }
-  // },[account])
+  }
+  
+    // useEffect(() => {
+      
+    //   if(account.statusCode === 200){
+    //     alert("회원가입이 완료되었습니다.")
+    //     setJoin({
+    //       userId : "",
+    //       nickname: "",
+    //       password: "",
+    //       passwordCheck: "",
+    //     })
+    //       window.location.replace("/SignIn")
+    //   }
+    // },[account])
+
   return (
     <SignupContainer>
       <SignupBox onSubmit={onSubmitHandler}>
         <LogoBox>
+
         </LogoBox>
           <InputBox>
-        <LogoBox></LogoBox>
-        <InputBox>
           <FlexInput>
-            <Input
-              name="email"
-              placeholder="이메일 형식을 입력해주세요 "
-              onChange={onChangeHandler}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                dispatch(__userCheck({ email: join.email }));
-              }}
-            >
-              중복확인
-            </button>
-            <ErrorMessageWrap>
-              {!IdValid
-                ? !IdValid &&
-                  join.email.length > 0 && <div>이메일 형식을 입력해주세요</div>
-                : IdValid &&
-                  join.email.length > 0 && (
+              <Input
+                name='userId'
+                placeholder='이메일 형식을 입력해주세요 '
+                onChange={onChangeHandler}
+              /> 
+
+              <button onClick={()=>{dispatch(__userCheck({userId:join.userId}))}}
+                >중복확인
+              </button>
+              <ErrorMessageWrap>
+              { !IdValid ?
+                  !IdValid && join.userId.length > 0 && (
+                    <div>이메일 형식을 입력해주세요</div>
+                  )
+                  :
+                  IdValid && join.userId.length > 0 && (
                     <Green>올바른 이메일 형식 입니다.</Green>
                   )
-                  )}
-            </ErrorMessageWrap>
+              }  
+              </ErrorMessageWrap>
           </FlexInput>
           <FlexInput>
-            <Input
-              name="nickname"
-              placeholder="닉네임 영문 또는 숫자 _기호 2자~20자 이하"
-              onChange={onChangeHandler}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                dispatch(__NickCheck({ nickname: join.nickname }));
-              }}
-            >
-              중복확인
-            </button>
-            <ErrorMessageWrap>
-              {!nickValid
-                ? !nickValid &&
-                  join.nickname.length > 0 && (
-                    <div>닉네임 영문,한글,숫자,기호 특수문자(_) 2자~20자</div>
+              <Input
+                name='nickname'
+                placeholder='닉네임 영문 또는 숫자 _기호 2자~20자 이하'
+                onChange={onChangeHandler}
+              />
+
+
+              <button onClick={()=>{dispatch(__NickCheck({nickname:join.nickname}))}}
+                >중복확인</button>
+              <ErrorMessageWrap>
+                {
+                  !nickValid ?
+                  !nickValid && join.nickname.length > 0 && (
+                  <div>닉네임 영문,한글,숫자,기호 특수문자(_) 2자~20자</div>
                   )
                 : nickValid &&
                   join.nickname.length > 0 && (
                     <Green>올바른 닉네임 형식 입니다.</Green>
-                  )}
-            </ErrorMessageWrap>
-          </FlexInput>
-          <FlexInput>
-            <Input
-              placeholder="비밀번호는 영문 숫자 특수기호 포함 8자~20자 이하 "
-              type="password"
-              name="password"
-              onChange={onChangeHandler}
-            />
-            <ErrorMessageWrap>
-              {!PwValid
-                ? !PwValid &&
-                  join.password.length > 0 && (
-                    <div>비밀번호 영문 숫자 특수기호 포함 8자~20자</div>
+                    )
+                  
+                }  
+                </ErrorMessageWrap>
+            </FlexInput>
+
+            <FlexInput>
+                <Input
+                  placeholder='비밀번호는 영문 숫자 특수기호 포함 8자~20자 이하 '
+                  type='password'
+                  name='password'
+                  onChange={onChangeHandler}
+                />
+                  <ErrorMessageWrap>
+                {
+                  !PwValid?
+
+                  !PwValid && join.password.length > 0 && (
+                  <div>비밀번호 영문 숫자 특수기호 포함 8자~20자</div>
                   )
                 : PwValid &&
                   join.password.length > 0 && (
                     <Green>사용 가능한 비밀번호 입니다.</Green>
-                  )}
-            </ErrorMessageWrap>
-          </FlexInput>
-          <FlexInput>
-            <Input
-              placeholder="비밀번호는 영문 숫자 특수기호 포함 8자~20자 이하"
-              type="password"
-              name="passwordCheck"
-              onChange={onChangeHandler}
-            />
-            <ErrorMessageWrap>
-              {!PwCValid
-                ? !PwCValid &&
-                  join.passwordCheck.length > 0 && (
+                    )
+
+                }  
+                </ErrorMessageWrap>
+            </FlexInput>
+            
+            <FlexInput>
+              
+              <Input
+                placeholder='비밀번호는 영문 숫자 특수기호 포함 8자~20자 이하'
+                type='password'
+                name='passwordCheck'
+                onChange={onChangeHandler}
+              />
+              <ErrorMessageWrap>
+                  {
+                    !PwCValid ? 
+                    !PwCValid && join.passwordCheck.length > 0 && (
                     <div>비밀번호 영문 숫자 특수기호 포함 8자~20자</div>
-                  )
-                : PwCValid &&
-                  join.passwordCheck.length > 0 &&
-                  (join.passwordCheck === join.password ? (
-                    <Green>사용 가능한 비밀번호 입니다.</Green>
-                  ) : (
-                    <div>비밀번호가 일치하지 않습니다.</div>
-                  ))
-                  //위에 비밀번호와 일치하는지 대조.
-              }
-            </ErrorMessageWrap>
-          </FlexInput>
-          <SignupButton onClick={onSubmitHandler}>회원가입</SignupButton>
-        </InputBox>
+                    )
+                    :
+                    PwCValid && join.passwordCheck.length > 0  && (
+                      
+                      join.passwordCheck == join.password? 
+                      <Green>사용 가능한 비밀번호 입니다.</Green>
+                      :
+                      <div>비밀번호가 일치하지 않습니다.</div>
+                      //위에 비밀번호와 일치하는지 대조.
+                    )
+
+                  }  
+              </ErrorMessageWrap>
+
+            </FlexInput>
+            <SignupButton onClick={onSubmitHandler}>회원가입</SignupButton>
+          </InputBox>
       </SignupBox>
       <LoginBox>
         {" "}
@@ -236,16 +249,21 @@ const SignUp = () => {
     </SignupContainer>
   );
 };
+
 export default SignUp;
+
 const Green = styled.div`
-  color: green;
-`;
-const ErrorMessageWrap = styled.div`
-  margin: 4px;
-  color: #ef0000;
-  font-size: 6px;
-`;
-const FlexInput = styled.span``;
+color:green;
+`
+
+const ErrorMessageWrap =styled.div`
+margin:4px;
+color:#ef0000;
+font-size:6px;
+`
+
+const FlexInput = styled.span``
+
 const SignupContainer = styled.div`
   width: 100%;
   height: 100vh;
@@ -254,15 +272,18 @@ const SignupContainer = styled.div`
   justify-content: center;
   flex-direction: column;
 `;
+
 const SignupBox = styled.form`
   background-color: white;
   width: 350px;
+
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   padding-bottom: 36px;
 `;
+
 const InputBox = styled.div`
   margin-top: 36px;
   display: flex;
@@ -270,8 +291,10 @@ const InputBox = styled.div`
   justify-content: center;
   flex-direction: column;
 `;
+
 const Input = styled.input`
   border: none;
+
   width: 250px;
   height: 40px;
   margin-bottom: 8px;
@@ -283,7 +306,9 @@ const Input = styled.input`
     outline: 1px solid #ADADAD;
   }
 `;
+
 const SignupButton = styled.button`
+
   color: white;
   border: none;
   border-radius: 5px;
@@ -296,6 +321,7 @@ const SignupButton = styled.button`
     background-color: #B2DFFC;
   }
 `;
+
 const LogoBox = styled.div`
   width: 175px;
   height: 51px;
@@ -306,22 +332,28 @@ const LogoBox = styled.div`
     height: 100%;
   }
 `;
+
 const SignupText = styled.div`
   width: 200px;
   text-align: center;
   font-size: 17px;
   line-height: 20px;
   font-weight: bold;
+
 `;
+
+
 const LoginBox = styled.div`
   background-color: white;
   width: 350px;
   padding: 20px;
+
   margin-top: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   span {
+
     margin-left: 4px;
     font-weight: bold;
     // 버튼 누르면 손모양 나오게 하는 마우스 커서
