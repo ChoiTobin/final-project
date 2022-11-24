302886d870ca91dfe31315a9bee00ab6cbed0435
minzzjo <minxxjozz@gmail.com>
2022-11-24 15:33:18 +0900

    css 최종수정 준비중이고 프로필사진 업로드 fix완료

--- src/fonts/globalFont.js
+++ src/fonts/globalFont.js
@@ -1,47 +1,58 @@
 import { createGlobalStyle } from "styled-components";
-import SFProBlack from "./SF-Pro-Text-Black.otf";
-import SFProBold from "./SF-Pro-Text-Bold.otf";
-import SFProHeavy from "./SF-Pro-Text-Heavy.otf";
-import SFProLight from "./SF-Pro-Text-Light.otf";
-import SFProMedium from "./SF-Pro-Text-Medium.otf";
-import SFProSemiBold from "./SF-Pro-Text-Semibold.otf";
 
 const GlobalFont = createGlobalStyle`
   @font-face {
-    font-family: 'SFProBlack';
-    src: local('SFProBlack'), local('SFProBlack');
-    font-style: black;
-    src: url(${SFProBlack}) format('opentype');
+	font-family: "inter";
+	src: url("./fonts/Inter-Black.woff");
+  font-weight: 900;
   }
+
   @font-face {
-    font-family: 'SFProBold';
-    src: local('SFProBold'), local('SFProBold');
-    font-style: bold;
-    src: url(${SFProBold}) format('opentype');
+    font-family: "inter";
+    src: url("./fonts/Inter-ExtraBold.woff");
+    font-weight: 800;
   }
+
   @font-face {
-    font-family: 'SFProHeavy';
-    src: local('SFProHeavy'), local('SFProHeavy');
-    font-style: black;
-    src: url(${SFProHeavy}) format('opentype');
+    font-family: "inter";
+    src: url("./fonts/Inter-Bold.woff");
+    font-weight: 700;
   }
+
   @font-face {
-    font-family: 'SFProLight';
-    src: local('SFProLight'), local('SFProLight');
-    font-style: thin;
-    src: url(${SFProLight}) format('opentype');
+    font-family: "inter";
+    src: url("./fonts/Inter-SemiBold.woff");
+    font-weight: 600;
   }
+
   @font-face {
-    font-family: 'SFProMedium';
-    src: local('SFProMedium'), local('SFProMedium');
-    font-style: initial;
-    src: url(${SFProMedium}) format('opentype');
+    font-family: "inter";
+    src: url("./fonts/Inter-Medium.woff");
+    font-weight: 500;
   }
+
   @font-face {
-    font-family: 'SFProSemiBold';
-    src: local('SFProSemiBold'), local('SFProSemiBold');
-    font-style: medium;
-    src: url(${SFProSemiBold}) format('opentype');
+    font-family: "inter";
+    src: url("./fonts/Inter-Regular.woff");
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "inter";
+    src: url("./fonts/Inter-Light.woff");
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: "inter";
+    src: url("./fonts/Inter-ExtraLight.woff");
+    font-weight: 200;
+  }
+
+  @font-face {
+    font-family: "inter";
+    src: url("./fonts/Inter-Thin.woff");
+    font-weight: 100;
   }
 `;
 
