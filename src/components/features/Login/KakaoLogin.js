20b6afe33e6d8528e657a628b5058d35852e0746
rorobin <87680494+ChoiTobin@users.noreply.github.com>
2022-12-03 11:34:11 +0900

    Update KakaoLogin.js

--- src/components/features/Login/KakaoLogin.js
+++ src/components/features/Login/KakaoLogin.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import styled from "styled-components"
-import { ReactComponent as Naver } from "../../../img/naverLogo.svg";
-import { ReactComponent as Kakao } from "../../../img/kakaoLogo.svg";
+import { ReactComponent as Naver } from "../../../img/naver.svg";
+import { ReactComponent as Kakao } from "../../../img/kakao.svg";
+import { useNavigate } from 'react-router-dom'
 
 const KakaoLogin = () => {
+    const navigate  = useNavigate();
     const REST_API_KEY = process.env.REACT_APP_API_KAKAO_ID
     const REDIRECT_URI = process.env.REACT_APP_API_URL
-    //console.log(REDIRECT_URI,REST_API_KEY,"확인")
-    // const location = useLocation();
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
     const handleLogin = () => {
         window.location.href = KAKAO_AUTH_URL;
     }
+    //kakao
+    const handleNaverLogin = () => {
+        let NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_NAVER_CALLBACK_URL}&state=state`
+        window.location.href = NAVER_AUTH_URL
+      }
+
+    //naver
+
     return (
-    <div>
-        {/* <Img onClick={()=>alert("준비중 입니다.")} src={require("../../img/NAVERLOGO.png")} alt="네이버 로그인 버튼"  /> */}
-        <Naver onClick={()=>alert("준비중 입니다.")} />
-        <a id="kakao-login-btn" onClick={handleLogin}>
-        {/* <Img2 src={require("../../img/kakao.png")}alt="카카오 로그인 버튼" /> */}
-        <Kakao/>
-        </a>
-        <p id="token-result"></p>
-    </div>
-    )
+        <div>
+            <Naver onClick={handleNaverLogin} />
+            <a id="kakao-login-btn" onClick={handleLogin}>
+            <Kakao/>
+            </a>
+            <p id="token-result"></p>
+        </div>
+        )
 }
 
 
