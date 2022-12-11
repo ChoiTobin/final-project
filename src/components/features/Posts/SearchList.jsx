02a9de5a515d3597f789b88329780a8d2ce8c888
minzzjo <116329960+minzzjo@users.noreply.github.com>
2022-12-11 17:05:38 +0900

    Update SearchList.jsx

--- src/components/features/Posts/SearchList.jsx
+++ src/components/features/Posts/SearchList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   __getPostTime,
   __getKeyword,
