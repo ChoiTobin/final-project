335873683ddc42c0564edbe8200910f858d3b152
minzzjo <116329960+minzzjo@users.noreply.github.com>
2022-12-11 17:06:34 +0900

    Update postSlice.js

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -4,7 +4,6 @@ import Apis from "../../shared/Apis"
 import axios from "axios";
 import { __putPost } from "./mypageSlice";	
 
-
 // 평점넘기기
 export const __getPostRating = createAsyncThunk(	
   "api/posts/getPostRating",	
